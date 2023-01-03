d624dc04ec097581a25f717edc3c65556c74b610
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2023-01-03 10:05:04 -0300

    CSS - Adicionado styles para site com API

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 let text_latitude = document.querySelector('.latitude')
 let text_longitude = document.querySelector('.longitude')
 
-const geolocation = navigator.geolocation;
+const geolocation = navigator.geolocation
 
 geolocation.getCurrentPosition((position) => {
-	let latitude = position.coords.latitude;
-	let longitude = position.coords.longitude;
+	let latitude = position.coords.latitude
+	let longitude = position.coords.longitude
 
-
+	// insere latitude e longitude na tela
 	text_latitude.insertAdjacentHTML("beforeEnd", latitude)
 	text_longitude.insertAdjacentHTML("beforeEnd", longitude)
+
+	//url do Google maps, pesquisando as coordenadas atuais
+	let google_maps = 'https://www.google.com.br/maps/search/' + latitude + ',' + longitude + '/'
+	
+	var link_gm = document.getElementById("google_maps")
+
+	// insere texto à tag a
+	link_gm.innerHTML = "Acesse seu endereço no Google Maps"
+	
+	// muda o href dessa tag
+	link_gm.setAttribute('href', google_maps)
 })
